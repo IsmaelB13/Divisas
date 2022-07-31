3700eebb5ee99b1a5da9e4ea178db537dbd66259
IsmaelB13 <ismaelballesteros9@gmail.com>
2022-07-31 10:42:33 -0500

    Initial commit

---
+++ src/divisas.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class divisas {
+    private JTextField origenTF;
+    private JTextField destinoTF;
+    private JButton convertirBTN;
+    private JComboBox origenCB;
+    private JComboBox destinoCB;
+    private JLabel destinotxt;
+    private JLabel origentxt;
+}
