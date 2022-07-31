4518f68f6eee08e7e6cf2e1803446eaa62ace47a
Andy Loor <andiloor2809@gmail.com>
2022-07-31 14:12:40 -0500

    Creacion de la estructura visual del convertidor de divisas

--- src/divisas.java
+++ src/divisas.java
@@ -24,7 +24,16 @@ public class divisas {
         convertirBTN.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                double[] tarifas={1,20.40,0.98,0.82,902.48};
+                double seleccion=tarifas[destinoCB.getSelectedIndex()];
+                double resultado=Double.parseDouble(origenTF.getText())*seleccion;
+                destinoTF.setText(""+resultado);
+            }
+        });
+        destinoCB.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                destinoTF.setText(""+destinoCB.getSelectedItem());
             }
         });
     }
