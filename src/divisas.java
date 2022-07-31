74a7212362dffc6fe161ffcfa5c115c99798598a
Andy Loor <andiloor2809@gmail.com>
2022-07-31 13:40:55 -0500

    Creacion de la estructura visual del convertidor de divisas

--- src/divisas.java
+++ src/divisas.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class divisas {
     private JTextField origenTF;
@@ -8,4 +10,30 @@ public class divisas {
     private JComboBox destinoCB;
     private JLabel destinotxt;
     private JLabel origentxt;
+    private JPanel mainForm;
+
+
+    public divisas() {
+        origenCB.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            destinoTF.setText(""+origenCB.getSelectedItem());
+            }
+        });
+
+        convertirBTN.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("divisas");
+        frame.setContentPane(new divisas().mainForm);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
