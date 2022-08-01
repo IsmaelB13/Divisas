df9288eaf5453d7c29482d45b93755c7aa8f4053
IsmaelB13 <ismaelballesteros9@gmail.com>
2022-08-01 00:14:27 -0500

    Divisas

--- src/divisas.java
+++ src/divisas.java
@@ -1,3 +1,4 @@
+//importacion de librerias necesarias
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,68 +12,66 @@ public class divisas {
     private JLabel destinotxt;
     private JLabel origentxt;
     private JPanel mainForm;
-
+    //creacion de la clase de divisas
     public divisas() {
+        //creacion de el actionlistener del origenbox
         origenCB.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 origenTF.setText(" " + destinoTF);
             }
         });
+        //creacion de el actionlistener del comvertidor
         convertirBTN.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 switch (origenCB.getSelectedIndex()) {
+                    case 0: {
+                        double[] usd = {1, 20.38, 0.92, 20.38, 901.10};
+                        double tarifaseleccionada = usd[destinoCB.getSelectedIndex()];
+                        double resultado = Double.parseDouble(origenTF.getText()) * tarifaseleccionada;
+                        destinoTF.setText(" " + resultado);
+                        break;
+                    }
                     case 1: {
-                        double[] usd = {1, 20.40, 0.98, 0.82, 902.48};
-                        double tarifaseleccionada = usd[destinoCB.getSelectedIndex()];                    //----> Creacion del primer metodo DE USD
-                        double resultado = Double.parseDouble(origenTF.getText()) * tarifaseleccionada;            //a el resto de monedas
-                        destinoTF.setText("" + resultado);
+                        double[] euro = {1.02, 1, 0.84, 20.85, 921.65};
+                        double tarifaseleccionada = euro[destinoCB.getSelectedIndex()];
+                        double resultado = Double.parseDouble(origenTF.getText()) * tarifaseleccionada;
+                        destinoTF.setText(" " + resultado);
                         break;
                     }
                     case 2: {
-                        double[] euro = {0.98,1,0.84,20.82,938.87};
-                        double tarifaseleccionada = euro[destinoCB.getSelectedIndex()];                   //---> Creacion del segundo metodo
-                        double resultado = Double.parseDouble(origenTF.getText()) * tarifaseleccionada;       //de Euro a las demas monedas
-                        destinoTF.setText("" + resultado);
+                        double[] libra = {1.22, 1.19, 1, 24.84, 1098.20};
+                        double tarifaseleccionada = libra[destinoCB.getSelectedIndex()];
+                        double resultado = Double.parseDouble(origenTF.getText()) * tarifaseleccionada;
+                        destinoTF.setText(" " + resultado);
                         break;
                     }
                     case 3: {
-                        double[] libra = {5.17, 1115.44, 0.25, 1, 0.36};
-                        double tarifaseleccionada = libra[destinoCB.getSelectedIndex()];
+                        double[] mxm = {0.049, 0.048, 0.040, 1, 44.22};
+                        double tarifaseleccionada = mxm[destinoCB.getSelectedIndex()];
                         double resultado = Double.parseDouble(origenTF.getText()) * tarifaseleccionada;
-                        destinoTF.setText("" + resultado);
+                        destinoTF.setText(" " + resultado);
                         break;
                     }
                     case 4: {
-                        double[] mxm = {14.20, 3056.61, 0.70, 2.74, 1};
-                        double tarifaseleccionada = mxm[destinoCB.getSelectedIndex()];
-                        double resultado = Double.parseDouble(origenTF.getText()) * tarifaseleccionada;
-                        destinoTF.setText("" + resultado);
-                        break;
-                    }
-                    case 5: {
-                        double[] chile = {14.20, 3056.61, 0.70, 2.74, 1};
+                        double[] chile = {0.0011, 0.0011, 0.00091, 0.023,1};
                         double tarifaseleccionada = chile[destinoCB.getSelectedIndex()];
                         double resultado = Double.parseDouble(origenTF.getText()) * tarifaseleccionada;
                         destinoTF.setText(" " + resultado);
                         break;
                     }
                 }
-                }
+            }
 
         });
     }
-        public static void main (String[]args){
-            JFrame frame = new JFrame("divisas");
-            frame.setContentPane(new divisas().mainForm);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.pack();
-            frame.setVisible(true);
-        }
-
+    public static void main (String[]args){
+        JFrame frame = new JFrame("divisas");
+        frame.setContentPane(new divisas().mainForm);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 
-
-
-
+}
