e9db89399d4aa1b8c6061474a05c8f1521f87f52
Andy Loor <andiloor2809@gmail.com>
2022-07-31 22:37:11 -0500

    Crecion de cada uno de los caso para las diferentes monedas

--- src/divisas.java
+++ src/divisas.java
@@ -12,37 +12,63 @@ public class divisas {
     private JLabel origentxt;
     private JPanel mainForm;
 
-
     public divisas() {
         origenCB.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-            destinoTF.setText(""+origenCB.getSelectedItem());
+                origenTF.setText(" " + destinoTF);
             }
         });
-
         convertirBTN.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                double[] tarifas={1,20.40,0.98,0.82,902.48};
-                double seleccion=tarifas[destinoCB.getSelectedIndex()];
-                double resultado=Double.parseDouble(origenTF.getText())*seleccion;
-                destinoTF.setText(""+resultado);
+                switch (origenCB.getSelectedIndex()){
+                    case 1:
+                    {
+                        double[] tarifasMexico={1,};
+                        double tarifaseleccionada=tarifasMexico[destinoCB.getSelectedIndex()];
+                        double resultado=Double.parseDouble(origenTF.getText())*tarifaseleccionada;
+                        destinoTF.setText(""+resultado);
+                        break;
+                    }
+                    case 2:
+                    {
+                        double[] tarifasColombia={0.0046 ,1,0.00023,0.00089,0.00033 };
+                        double tarifaseleccionada=tarifasColombia[destinoCB.getSelectedIndex()];
+                        double resultado=Double.parseDouble(origenTF.getText())*tarifaseleccionada;
+                        destinoTF.setText(""+resultado);
+                        break;
+                    }
+                    case 3:
+                    {
+                        double[] tarifasSOl={5.17,1115.44 ,0.25 ,1,0.36  };
+                        double tarifaseleccionada=tarifasSOl[destinoCB.getSelectedIndex()];
+                        double resultado=Double.parseDouble(origenTF.getText())*tarifaseleccionada;
+                        destinoTF.setText(""+resultado);
+                        break;
+                    }
+                    case 4:
+                    {
+                        double[] tarifasAustralia={14.20,3056.61,0.70,2.74 ,1};
+                        double tarifaseleccionada=tarifasAustralia[destinoCB.getSelectedIndex()];
+                        double resultado=Double.parseDouble(origenTF.getText())*tarifaseleccionada;
+                        destinoTF.setText(""+resultado);
+                        break;
+                    }
+                }
             }
         });
-        destinoCB.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                destinoTF.setText(""+destinoCB.getSelectedItem());
-            }
-        });
+    }
+        public static void main (String[]args){
+            JFrame frame = new JFrame("divisas");
+            frame.setContentPane(new divisas().mainForm);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+        }
+
     }
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("divisas");
-        frame.setContentPane(new divisas().mainForm);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-    }
-}
+
+
+
